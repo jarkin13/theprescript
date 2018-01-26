0238b896e54447fa335cb21a3cf733e4d1912559
Jamie Arkin <jamielaurenarkin@gmail.com>
2018-01-25 23:29:10 -0500

    add font to homepage

--- wp-content/themes/mesmerize/header-homepage.php
+++ wp-content/themes/mesmerize/header-homepage.php
@@ -4,6 +4,7 @@
     <meta charset="<?php bloginfo('charset'); ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="profile" href="http://gmpg.org/xfn/11">
+    <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
     <link rel="stylesheet" href="https://use.typekit.net/gve7oml.css">
     <?php wp_head(); ?>
 </head>
