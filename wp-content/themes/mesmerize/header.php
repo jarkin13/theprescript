475e4555c42fea1c086931e411eb49337bc8d1b3
Jamie Arkin <jamielaurenarkin@gmail.com>
2018-02-27 15:46:24 -0500

    escape quotes from facebook header content

--- wp-content/themes/mesmerize/header.php
+++ wp-content/themes/mesmerize/header.php
@@ -14,8 +14,8 @@
     ?>
     <meta property="og:url"           content="<?php echo get_permalink(); ?>" />
     <meta property="og:type"          content="website" />
-    <meta property="og:title"         content="<?php echo get_the_title(); ?>" />
-    <meta property="og:description"   content="<?php echo substr($content, 0, 300); ?>..." />
+    <meta property="og:title"         content="<?php echo addslashes(get_the_title()); ?>" />
+    <meta property="og:description"   content="<?php echo addslashes(substr($content, 0, 300)); ?>..." />
     <meta property="og:image"         content="<?php echo the_post_thumbnail_url(); ?>" />
   <?php endif; ?>
 </head>
